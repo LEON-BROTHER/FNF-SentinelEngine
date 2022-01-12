5ec9aa0981db73e1a505f9ce8411474d40901754
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-01-12 17:21:12 +0000

    Window

---
+++ src/Main.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.FPS;
+import openfl.Lib;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+    public static function main():Void
+    {
+        Lib.current.addChild(new Main());
+    }
+
+    public function new()
+    {
+        super();
+
+        addChild(new FlxGame(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight, SignInState, 1, 60, 60, true, false));
+        addChild(new FPS(5, 5, 0xFFFFFF));
+    }
+}
---
+++ src/SignInState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class SignInState extends FlxState
+{
+    public function new()
+    {
+        super();
+    }
+
+    override function create()
+    {
+        super.create();
+    }
+}
