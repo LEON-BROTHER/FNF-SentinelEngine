e2e06c3040ce3a2568701f10ab00996f75642d2b
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-01-16 10:39:00 +0000

    Update TitleState.hx

--- src/TitleState.hx
+++ src/TitleState.hx
@@ -21,13 +21,18 @@ class TitleState extends FlxState
             FlxG.sound.music.fadeIn(4, 0, 1);
         }
 
-        var gf:FlxSprite = new FlxSprite();
-        gf.x = FlxG.width * 0.4;
-        gf.y = FlxG.height * 0.07;
+        var gf:FlxSprite = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
         gf.frames = Files.getSparrowAtlas("gfDanceTitle", "preload");
         gf.animation.addByPrefix("dance", "gfDance", 24, true);
         gf.animation.play("dance");
         gf.antialiasing = Config.antialiasing;
         add(gf);
+
+        var titleCard:FlxSprite = new FlxSprite(-100, -100);
+        titleCard.frames = Files.getSparrowAtlas("logoBumpin", "preload");
+        titleCard.animation.addByPrefix("logo", "logo bumpin", 24, true);
+        titleCard.animation.play("logo");
+        titleCard.antialiasing = Config.antialiasing;
+        add(titleCard);
     }
 }
