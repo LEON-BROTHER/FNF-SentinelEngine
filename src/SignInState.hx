8188345780fd10246d13239968b4e92a7d5e6ff7
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-01-16 06:59:58 +0000

    Big Boi!

--- src/SignInState.hx
+++ src/SignInState.hx
@@ -33,21 +33,33 @@ class SignInState extends FlxState
 
         var logIn:FlxButton = new FlxButton(0, 0, "Log In", login);
         logIn.setSize(310, 140);
+        logIn.scale.x = 4;
+        logIn.scale.y = 2;
+        logIn.label.fieldWidth *= 4;
         logIn.updateHitbox();
+        logIn.label.setFormat(Files.font("vcr.ttf"), 24, FlxColor.GRAY, FlxTextAlign.CENTER, FlxTextBorderStyle.NONE, FlxColor.TRANSPARENT, true);
         logIn.screenCenter();
         add(logIn);
         logIn.y -= 100;
 
         var signUp:FlxButton = new FlxButton(0, 0, "Sign Up", signup);
         signUp.setSize(310, 140);
+        signUp.scale.x = 4;
+        signUp.scale.y = 2;
+        signUp.label.fieldWidth *= 4;
         signUp.updateHitbox();
+        signUp.label.setFormat(Files.font("vcr.ttf"), 24, FlxColor.GRAY, FlxTextAlign.CENTER, FlxTextBorderStyle.NONE, FlxColor.TRANSPARENT, true);
         signUp.screenCenter();
         add(signUp);
         signUp.y += 100;
 
         var skip:FlxButton = new FlxButton(0, 0, "Skip Login", skipLogin);
         skip.setSize(310, 140);
+        skip.scale.x = 4;
+        skip.scale.y = 2;
+        skip.label.fieldWidth *= 4;
         skip.updateHitbox();
+        skip.label.setFormat(Files.font("vcr.ttf"), 24, FlxColor.GRAY, FlxTextAlign.CENTER, FlxTextBorderStyle.NONE, FlxColor.TRANSPARENT, true);
         skip.screenCenter();
         add(skip);
         skip.y += 210;
