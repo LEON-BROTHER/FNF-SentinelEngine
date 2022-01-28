9c269c4aa31a59118cf9f7a8d993e1ff049a1aec
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-01-28 16:34:04 +0000

    Update Stage.hx

--- src/Stage.hx
+++ src/Stage.hx
@@ -14,15 +14,27 @@ class Stage extends FlxTypedGroup<FlxSprite>
             default:
                 var bg:FlxSprite = new FlxSprite().loadGraphic(Files.image("stageback", "shared"));
                 bg.antialiasing = Config.antialiasing;
-                bg.setGraphicSize(Std.int(bg.width * 2), Std.int(bg.height * 2));
+                bg.setGraphicSize(Std.int(bg.width * 1.1), Std.int(bg.height * 1.1));
+                bg.scrollFactor.set(0.8, 0.8);
                 bg.screenCenter();
                 add(bg);
-                //var front:FlxSprite = new FlxSprite().loadGraphic(Files.image("stagefront", "shared"));
-                //front.antialiasing = Config.antialiasing;
-                //add(front);
-                //var curtain:FlxSprite = new FlxSprite().loadGraphic(Files.image("stagecurtains", "shared"));
-                //curtain.antialiasing = Config.antialiasing;
-                //add(curtain);
+
+                var front:FlxSprite = new FlxSprite().loadGraphic(Files.image("stagefront", "shared"));
+                front.antialiasing = Config.antialiasing;
+                front.setGraphicSize(Std.int(bg.width * 1.1), Std.int(bg.height * 1.1));
+                front.scrollFactor.set(0.9, 0.9);
+                front.screenCenter();
+                add(front);
+
+                if (!Config.lowquality)
+                {
+                    var curtain:FlxSprite = new FlxSprite().loadGraphic(Files.image("stagecurtains", "shared"));
+                    curtain.antialiasing = Config.antialiasing;
+                    curtain.setGraphicSize(Std.int(bg.width * 1.1), Std.int(bg.height * 1.1));
+                    curtain.scrollFactor.set(0.9, 0.9);
+                    curtain.screenCenter();
+                    add(curtain);
+                }
         }
     }
 }
