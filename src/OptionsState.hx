567010008fcd9b3d6a5564532b827068af11a6c8
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-01-31 18:23:16 +0000

    Update OptionsState.hx

--- src/OptionsState.hx
+++ src/OptionsState.hx
@@ -5,6 +5,13 @@ import flixel.FlxState;
 
 class OptionsState extends FlxState
 {
+    private var options:Array<Dynamic> = [
+        ["Anti-Aliasing", BoolOptionType, "Enable/Disable Anti-Aliasing, Which Smoothes Out The Texture's To Make Them Look Less Pixelly"],
+        ["Low Quality", BoolOptionType, "Enabling Increase's Performance, But Make's The Game Not Look As Good"],
+        ["Record Replay's", BoolOptionType, "Enable/Disable Whether You Want The Game To Record Replay's"],
+        ["Cache Replay File's", BoolOptionType, "Enable/Disable Whether You Want Your Replay To Have The Nessecary File's Stored Inside It (Song, Chart, Character's). Useful If You Want To Send Your Friend A Replay For A Mod Song."]
+    ];
+
     public function new()
     {
         super();
@@ -19,5 +26,25 @@ class OptionsState extends FlxState
             FlxG.sound.play(Files.sound("cancelMenu", "preload"));
             FlxG.switchState(new MainMenuState());
         }
+    }
+}
+
+class OptionType
+{
+    public var title:String;
+    public var description:String;
+
+    public function new(titl:String, desc:String)
+    {
+        title = titl;
+        description = desc;
+    }
+}
+
+class BoolOptionType extends OptionType
+{
+    public function new(titl:String, desc:String)
+    {
+        super(titl, desc);
     }
 }
