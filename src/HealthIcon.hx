de93db7c68122c70a98216f2789c01c330a3a21c
Generalisk <80116907+Generalisk@users.noreply.github.com>
2022-02-13 11:46:13 +0000

    Update HealthIcon.hx

--- src/HealthIcon.hx
+++ src/HealthIcon.hx
@@ -1,5 +1,9 @@
 package;
 
+#if sys
+import sys.FileSystem;
+#end
+
 import flixel.FlxSprite;
 
 class HealthIcon extends FlxSprite
@@ -9,7 +13,18 @@ class HealthIcon extends FlxSprite
         super();
 
         antialiasing = Config.antialiasing;
-        loadGraphic(Files.image("icons/icon-" + character, "preload"), true, 150, 150);
+        #if sys
+        if (FileSystem.exists(Files.image("icons/icon-" + character, "preload")))
+        #else
+        if (Assets.exists(Files.image("icons/icon-" + character, "preload")))
+        #end
+        {
+            loadGraphic(Files.image("icons/icon-" + character, "preload"), true, 150, 150);
+        }
+        else
+        {
+            loadGraphic(Files.image("icons/icon-null", "preload"), true, 150, 150);
+        }
         animation.pause();
 
         neutral();
